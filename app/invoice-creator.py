81ccb98d1786614fc43eeb9fdc2bfe065ac2d99c
Sahil Goonraz <sahilgoonrazk@gmail.com>
2024-06-05 15:16:18 +0100

    further refactorisation in quickstart.py to speed up code execution

--- app/invoice-creator.py
+++ app/invoice-creator.py
@@ -2,4 +2,4 @@ import datetime
 from quickstart import create_pdf
 
 if __name__ == "__main__":
-  create_pdf(month=5, year=2024)
+  create_pdf()
--- app/quickstart.py
+++ app/quickstart.py
@@ -1,224 +1,177 @@
 import datetime
-import os.path
-import google
+import os
+import json
 import calendar
 import jinja2
 import pdfkit
-import json
-
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
+# Constants
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 CREDENTIALS_FILE = "credentials.json"
 TOKEN_FILE = "token.json"
+USER_SETTINGS_FILE = 'user_settings.json'
+HTML_TEMPLATE_FILE = 'invoice.html'
+PDF_OUTPUT_FILE = 'invoice.pdf'
+CSS_FILE = 'static/css/invoice_styles.css'
+HEAD_TEMPLATE = 'templates/head.html'
+TAIL_TEMPLATE = 'templates/tail.html'
+NEW_TABLE_TEMPLATE = 'templates/new_table.html'
 
+# Fetch user settings
 def get_user_settings():
-  if os.path.isfile('user_settings.json'):
-    user_settings = json.load(open('user_settings.json', 'r'))
+    if os.path.isfile(USER_SETTINGS_FILE):
+        with open(USER_SETTINGS_FILE, 'r') as file:
+            user_settings = json.load(file)
+    else:
+        required_settings = ["full_name", "email", "address", "town", "postcode",
+                             "control_str", "hourly_rate", "account_no", "sort_code"]
+        
+        user_settings = {key: input(f"Please enter {' '.join(key.split('_'))}: ") for key in required_settings}
 
-  else:
-    required_settings = ["full_name", "email",
-                         "address", "town",
-                         "postcode", "control_str",
-                         "hourly_rate", "account_no", "sort_code"]
-    
-    user_settings = {}
-    
-    for key in required_settings:
-       user_settings[key] = input("Please enter {}: ".format(" ".join(key.split('_'))))
+        with open(USER_SETTINGS_FILE, 'w') as outfile:
+            json.dump(user_settings, outfile)
 
-    with open('user_settings.json', 'w') as outfile:
-       json.dump(user_settings, outfile)
-
-  return user_settings
+    return user_settings
 
 USER_SETTINGS = get_user_settings()
 
+# Fetch Google credentials
 def get_google_credentials():
-  creds = None
+    creds = None
+    if os.path.exists(TOKEN_FILE):
+        creds = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
 
-  if os.path.exists(TOKEN_FILE):
-    creds = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
-  
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
+            creds = flow.run_local_server(port=0)
 
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
-      creds = flow.run_local_server(port=0)
+        with open(TOKEN_FILE, "w") as token:
+            token.write(creds.to_json())
 
-    with open(TOKEN_FILE, "w") as token:
-      token.write(creds.to_json())
+    return creds
 
-  return creds
-
+# Fetch lessons info
 def get_lessons_info(month, year):
-  """
-  Returns a list containing different info partaining to the lessons
-  taught in a given month/year.
+    creds = get_google_credentials()
 
-  Arguments:
-    month (int)       : month for which to create dictionary for (e.g. for March enter 3).
-    year (int)        : year for which to create dictionary for (e.g. for 2024 enter 2024).
-    control_str (str) : the function will check for the specified string in the event name, and only treat
-                        it as a lesson if the control string is present. The control string will be removed
-                        from the event name, leaving only the student's name.
+    try:
+        service = build("calendar", "v3", credentials=creds)
+        time_min = datetime.datetime(year, month, 1).isoformat() + "Z"
+        time_max = datetime.datetime(year, month + 1, 1).isoformat() + "Z"
 
-  Returns:
-    lessons_info (list : list containing sublists made of a lesson's date, student and duration.
-  """
-  creds = get_google_credentials()
+        events_result = service.events().list(
+            calendarId="primary", timeMin=time_min, timeMax=time_max, maxResults=1000, singleEvents=True,
+            orderBy="startTime"
+        ).execute()
 
-  try:
-    service = build("calendar", "v3", credentials=creds)
-    lessons_info = []
-
-    events_result = (
-        service.events()
-        .list(
-            calendarId="primary",
-            timeMin=datetime.datetime(year, month, 1).isoformat() + "Z",
-            timeMax=datetime.datetime(year, month+1, 1).isoformat() + "Z",
-        )
-        .execute()
-    )
-
-    events = events_result.get("items", [])
-    control_str = USER_SETTINGS["control_str"]
-
-    # Gather data for list
-    for event in events:
-      # Get student name
-      student = get_student_name(control_str, event)
+        events = events_result.get("items", [])
+        control_str = USER_SETTINGS["control_str"]
         
-      # Get lesson date, time and duration
-      start = event["start"].get("dateTime", event["start"].get("date"))
-      start = datetime.datetime.fromisoformat(start)
-      
-      end = event["end"].get("dateTime", event["end"].get("date"))
-      end = datetime.datetime.fromisoformat(end)
+        lessons_info = [
+            [datetime.datetime.fromisoformat(event["start"].get("dateTime", event["start"].get("date"))).date(),
+             get_student_name(control_str, event),
+             (datetime.datetime.fromisoformat(event["end"].get("dateTime", event["end"].get("date"))).time().hour -
+              datetime.datetime.fromisoformat(event["start"].get("dateTime", event["start"].get("date"))).time().hour +
+              (datetime.datetime.fromisoformat(event["end"].get("dateTime", event["end"].get("date"))).time().minute -
+               datetime.datetime.fromisoformat(event["start"].get("dateTime", event["start"].get("date"))).time().minute) / 60)]
+            for event in events
+        ]
 
-      duration = end.time().hour - start.time().hour + (end.time().minute - start.time().minute) / 60
+        return lessons_info
+    
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return []
 
-      # Adds info to list 
-      lessons_info.append([start.date(), student, duration])
-
-    return lessons_info
-
-  except HttpError as error:
-    print(f"An error occurred: {error}")
-
+# Group lessons info by weeks
 def group_lessons(month, year):
-  lessons_info = get_lessons_info(month, year)
-  hourly_rate = int(USER_SETTINGS["hourly_rate"])
+    lessons_info = get_lessons_info(month, year)
+    hourly_rate = float(USER_SETTINGS["hourly_rate"])
 
-  for i in range(len(lessons_info)):
-    lessons_info[i].append(hourly_rate * lessons_info[i][2])
-    lessons_info[i].append(lessons_info[i][0].weekday())
-    lessons_info[i][0] = lessons_info[i][0].strftime('%d/%m/%Y')
+    for lesson in lessons_info:
+        lesson.append(hourly_rate * lesson[2])
+        lesson.append(lesson[0].weekday())
+        lesson[0] = lesson[0].strftime('%d/%m/%Y')
 
-  lessons_info.sort()
+    lessons_info.sort()
+    month_lessons = []
+    week = []
 
-  week = []
-  month_lessons = []
+    for i, lesson in enumerate(lessons_info):
+        week.append(lesson)
 
-  for i in range(len(lessons_info)):
-    week.append(lessons_info[i])
+        if i != len(lessons_info) - 1 and lesson[4] > lessons_info[i + 1][4]:
+            month_lessons.append(week)
+            week = []
 
-    if (i != len(lessons_info)-1) and (lessons_info[i][4] > lessons_info[i+1][4]):
-      month_lessons.append(week)
-      week = []
-    
-    elif i == len(lessons_info)-1:
-      month_lessons.append(week)
+        elif i == len(lessons_info) - 1:
+            month_lessons.append(week)
 
-  weekly_totals = []
+    weekly_totals = [sum(day[3] for day in week) for week in month_lessons]
 
-  for week in month_lessons:
-    week_total = 0
-    
-    for day in week:
-      week_total += day[3]
-      day[3] = "&pound{0:.2f}".format(day[3])
-    
-    weekly_totals.append(week_total)
+    for week in month_lessons:
+        for day in week:
+            day[3] = f"&pound{day[3]:.2f}"
 
-  return month_lessons, weekly_totals
+    return month_lessons, weekly_totals
 
+# Get student name from event
 def get_student_name(control_str, event):
-  if control_str in event["summary"]:
-    stripped_str = event["summary"][len(control_str):]
+    stripped_str = event["summary"].replace(control_str, "").strip()
     student, *_ = stripped_str.split(" ")
 
-  else:
-    student, *_ = event["summary"].split(" ")
+    return student
 
-  return student
-
+# Create HTML file for invoice
 def create_html(month_lessons, weekly_totals):
-  i = 0
+    hourly_rate = USER_SETTINGS["hourly_rate"]
 
-  head = open('templates/head.html', 'r')
-  tail = open('templates/tail.html', 'r')
-  new_table = open('templates/new_table.html', 'r').read()
-  hourly_rate = USER_SETTINGS["hourly_rate"]
+    with open(HEAD_TEMPLATE, 'r') as head_file, open(TAIL_TEMPLATE, 'r') as tail_file, open(NEW_TABLE_TEMPLATE, 'r') as table_file:
+        with open(HTML_TEMPLATE_FILE, 'w') as invoice_file:
+            invoice_file.write(head_file.read())
+            table_template = table_file.read()
 
-  with open('invoice.html', 'a') as f:
-      f.write(head.read())
-      
-      for week in month_lessons:
-          f.write(new_table)
-          for day in week:
-              f.write('\n<tr>')
-              f.write('\n<td>{}</td>'.format(day[0]))
-              f.write('\n<td>{}</td>'.format(day[1]))
-              f.write('\n<td>&pound{0:.2f}</td>'.format(int(hourly_rate)))
-              f.write('\n<td>{}</td>'.format(day[2]))
-              f.write('\n<td class="bold">{}</td>'.format(day[3]))
-              f.write('\n</tr>')
+            for i, week in enumerate(month_lessons):
+                invoice_file.write(table_template)
 
-          f.write('\n<tr>')
-          f.write('\n<td colspan="4" align="right" class="week-total"><strong>TOTAL DUE FOR WEEK {}</strong></td>'.format(i+1))
-          f.write('\n<td class="total"><strong>&pound{0:.2f}</strong></td>'.format(weekly_totals[i]))
-          f.write('\n')
-          i += 1
+                for day in week:
+                    invoice_file.write(f'\n<tr><td>{day[0]}</td><td>{day[1]}</td><td>&pound{hourly_rate:.2f}</td><td>{day[2]}</td><td class="bold">{day[3]}</td></tr>')
+                invoice_file.write(f'\n<tr><td colspan="4" align="right" class="week-total"><strong>TOTAL DUE FOR WEEK {i+1}</strong></td><td class="total"><strong>&pound{weekly_totals[i]:.2f}</strong></td></tr>')
+            
+            invoice_file.write('\n' + tail_file.read())
 
-      f.write('\n')
-      f.write(tail.read())
+# Create PDF invoice
+def create_pdf(month=None, year=None):
+    month = month or datetime.datetime.now().month
+    year = year or datetime.datetime.now().year
+    month_lessons, weekly_totals = group_lessons(month, year)
+    create_html(month_lessons, weekly_totals)
 
-def create_pdf(month = None, year = None):
-  month = month if month else datetime.datetime.now().month
-  year = year if year else datetime.datetime.now().year
+    context = {
+        'client_name': USER_SETTINGS['full_name'].title(),
+        'address_line1': USER_SETTINGS['address'].title(),
+        'address_line2': USER_SETTINGS['town'].title(),
+        'invoice_date': datetime.datetime.today().strftime("%d %b %Y"),
+        'address_line3': USER_SETTINGS['postcode'].upper(),
+        'invoice_no': f"{year}{month:02}",
+        'user_email': USER_SETTINGS["email"],
+        'account_no': USER_SETTINGS["account_no"],
+        'sort_code': USER_SETTINGS["sort_code"],
+        'monthly_total': f"{sum(weekly_totals):.2f}"
+    }
 
-  month_lessons, weekly_totals = group_lessons(month, year)
+    template_loader = jinja2.FileSystemLoader('./')
+    template_env = jinja2.Environment(loader=template_loader)
+    template = template_env.get_template(HTML_TEMPLATE_FILE)
+    output_text = template.render(context)
+    config = pdfkit.configuration(wkhtmltopdf='')
 
-  create_html(month_lessons, weekly_totals)
-
-  context = {'client_name': USER_SETTINGS['full_name'].title(),
-              'address_line1': USER_SETTINGS['address'].title(),
-              'address_line2': USER_SETTINGS['town'].title(),
-              'invoice_date': datetime.datetime.today().strftime("%d %b %Y"),
-              'address_line3': USER_SETTINGS['postcode'].upper(),
-              'invoice_no': year + month,
-              'user_email': USER_SETTINGS["email"],
-              'account_no': USER_SETTINGS["account_no"],
-              'sort_code': USER_SETTINGS["sort_code"],
-              'monthly_total': "{0:.2f}".format(sum(weekly_totals))
-  }
-
-  template_loader = jinja2.FileSystemLoader('./')
-  template_env = jinja2.Environment(loader=template_loader)
-
-  html_template = 'invoice.html'
-  template = template_env.get_template(html_template)
-  output_text = template.render(context)
-
-  config = pdfkit.configuration(wkhtmltopdf='')
-  output_pdf = 'invoice.pdf'
-  pdfkit.from_string(output_text, output_pdf, configuration=config, css='static/css/invoice_styles.css')
+    pdfkit.from_string(output_text, PDF_OUTPUT_FILE, configuration=config, css=CSS_FILE)
